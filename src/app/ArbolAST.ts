e65e50bd7f2da29bcdd7c2c4eef7ec10a6b978af
Luiskr1993 <luiskr1993@gmail.com>
2020-09-27 21:59:21 -0600

    [LV]:Carpeta dist

---
+++ src/app/ArbolAST.ts
@@ -0,0 +1,23 @@
+import { NodoArbol } from './NodoArbol';
+import { Component } from '@angular/core';
+import { tokenName } from '@angular/compiler';
+import { isNumber } from 'util';
+
+export class ArbolAST{
+    public raiz: NodoArbol;
+    public totalNodos: number;
+    constructor(){
+        this.raiz = null;
+        this.totalNodos = 0;
+    }
+    public NewNodo(id: string, valor: string): void{
+        let nodo: NodoArbol;
+        nodo = new NodoArbol(id, valor);
+        if (this.totalNodos === 0 ){
+            this.raiz = nodo;
+        } else {
+            this.totalNodos++;
+            this.raiz.insertaHijo(nodo.id, nodo.valor);
+        }
+    }
+}
